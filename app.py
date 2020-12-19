b6be393f65f4e8551c3bfc1bb42048053f8fc733
louisgg0104 <37336235+louisgg0104@users.noreply.github.com>
2020-12-20 00:40:30 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -13,9 +13,12 @@ from linebot.models import *
 from message import *
 from new import *
 from Function import *
+from bs4 import BeautifulSoup
 # ======這裡是呼叫的檔案內容=====
 
+
 # ======python的函數庫==========
+import requests
 import tempfile
 import os
 import datetime
@@ -26,13 +29,12 @@ app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 # Channel Access Token
 line_bot_api = LineBotApi(
-    'QQfRtT616WDzpgdI4nqi35t2yNOm1Wl7o38JgTkkUhX4LshwXxVCMyTE6i4rg7TuP7hV3h5R4wQmn1yvnVs+z2VGnHPqOODe4q42U5vzHFxaK9NC7kFE0sePKgaEFVHHd0T3xNEpZCab1AJ8FD0yTQdB04t89/1O/w1cDnyilFU=')
+    'QQfRtT616WDzpgdI4nqi35t2yNOm1Wl7o38JgTkkUhX4LshwXxVCMyTE6i4rg7TuP7hV3h5R4wQmn1yvnVs+z2VGnHPqOODe4q42U5vzHFxaK9NC7kFE0sePKgaEFVHHd0T3xNEpZCab1AJ8FD0yTQdB04t89/1O/w1cDnyilFU=                          ')
 # Channel Secret
 handler = WebhookHandler('0f0acb797b22a4bc0cdf6f76d927dc2a')
 
+
 # 監聽所有來自 /callback 的 Post Request
-
-
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -40,16 +42,16 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
+    # handle webhook body123
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
-
 # 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
+
+
+@ handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
     if '最新合作廠商' in msg:
@@ -71,8 +73,80 @@ def handle_message(event):
         message = function_list()
         line_bot_api.reply_message(event.reply_token, message)
     else:
-        message = TextSendMessage(text=msg)
+        message = TextSendMessage(
+            text='123132123')
+        # message = Carousel_Template()
         line_bot_api.reply_message(event.reply_token, message)
+
+
+# 旋轉木馬按鈕訊息介面
+
+def Carousel_Template():
+    message = TemplateSendMessage(
+        alt_text='一則旋轉木馬按鈕訊息',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Number_1_in_green_rounded_square.svg/200px-Number_1_in_green_rounded_square.svg.png',
+                    title='這是第一塊模板',
+                    text='一個模板可以有三個按鈕',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='回傳一個訊息',
+                            data='將這個訊息偷偷回傳給機器人'
+                        ),
+                        MessageTemplateAction(
+                            label='用戶發送訊息',
+                            text='我知道這是1'
+                        ),
+                        URITemplateAction(
+                            label='進入1的網頁',
+                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Number_1_in_green_rounded_square.svg/200px-Number_1_in_green_rounded_square.svg.png'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
+                    title='這是第二塊模板',
+                    text='副標題可以自己改',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='回傳一個訊息',
+                            data='這是ID=2'
+                        ),
+                        MessageTemplateAction(
+                            label='用戶發送訊息',
+                            text='我知道這是2'
+                        ),
+                        URITemplateAction(
+                            label='進入2的網頁',
+                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Number_2_in_light_blue_rounded_square.svg/200px-Number_2_in_light_blue_rounded_square.svg.png'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png',
+                    title='這是第三個模塊',
+                    text='最多可以放十個',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='回傳一個訊息',
+                            data='這是ID=3'
+                        ),
+                        MessageTemplateAction(
+                            label='用戶發送訊息',
+                            text='我知道這是3'
+                        ),
+                        URITemplateAction(
+                            label='uri2',
+                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png'
+                        )
+                    ]
+                )
+            ]
+        )
+    )
+    return message
 
 
 if __name__ == "__main__":
